59ffd3f597c54b4ad1b0f665d273b4c841313a3c
Kam <331677620@qq.com>
2023-02-07 11:20:36 +0800

    fix: 调整 fatherrc 配置，确保 es/lib 产物符合规范

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'father';
 export default defineConfig({
   esm: {
     output: 'es',
-    transformer: 'babel',
+    targets: { chrome: 49 },
   },
   cjs: {
     output: 'lib',
     transformer: 'babel',
+    targets: { ie: 11 },
     extraBabelPlugins: [
       [
         'babel-plugin-import',
