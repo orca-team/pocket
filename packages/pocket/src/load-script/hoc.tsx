c8ab372cd6d45879e9fe026c63bc25fdb171ccf7
wang.kangning <wb-wkn507465@alibaba-inc.com>
2023-02-13 20:49:26 +0800

    fix: withScript

--- packages/pocket/src/load-script/hoc.tsx
+++ packages/pocket/src/load-script/hoc.tsx
@@ -2,8 +2,8 @@ import type { ComponentType } from 'react';
 import React from 'react';
 import ReactScript from './ReactScript';
 
-export default function withScript<P>(src: string | string[]) {
-  return (Element: ComponentType<P>) => {
+export default function withScript(src: string | string[]) {
+  return function <P>(Element: ComponentType<P>) {
     const ComponentWithProps = React.forwardRef<Element, P>((props: P, ref) => (
       <ReactScript src={src}>
         <Element ref={ref} {...props} />
