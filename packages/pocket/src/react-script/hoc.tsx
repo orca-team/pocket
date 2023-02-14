8bbafd22fe38c0d69acbea70388f63866879bc06
Kam <331677620@qq.com>
2023-02-14 11:37:27 +0800

    fix: export withScript

--- packages/pocket/src/react-script/hoc.tsx
+++ packages/pocket/src/react-script/hoc.tsx
@@ -1,15 +1,20 @@
-import type { ComponentType } from 'react';
+import type { ComponentType, ForwardRefRenderFunction } from 'react';
 import React from 'react';
 import ReactScript from './ReactScript';
 
 export default function withScript(src: string | string[]) {
-  return function <P>(Element: ComponentType<P>) {
-    const ComponentWithProps = React.forwardRef<Element, P>((props: P, ref) => (
+  return function <
+    T,
+    P,
+    E extends ComponentType<P> | ForwardRefRenderFunction<T, P>,
+  >(Element: E) {
+    const ComponentWithProps = React.forwardRef<T, P>((props, ref) => (
       <ReactScript src={src}>
+        {/* @ts-expect-error */}
         <Element ref={ref} {...props} />
       </ReactScript>
     ));
 
-    return ComponentWithProps;
+    return ComponentWithProps as unknown as E;
   };
 }
