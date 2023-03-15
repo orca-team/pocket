6871b3f235070da5224449d6e494bd29f9e2d0e0
Kam <331677620@qq.com>
2023-03-15 11:09:49 +0800

    feat: PDFContext

--- packages/pdf-viewer/src/context.ts
+++ packages/pdf-viewer/src/context.ts
@@ -1,5 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-function */
 import React from 'react';
+import type { ShapeDataType } from '@orca-fe/painter';
+
+export type PDFViewerHandle = {
+  load: (file: string | URL | File | ArrayBuffer) => Promise<void>;
+  setZoom: (zoom: number) => void;
+  getZoom: () => number;
+  changePage: (pageIndex: number, anim?: boolean) => void;
+  scrollTo: Element['scrollTo'];
+  getCurrentPage: () => number;
+  getPageCount: () => number;
+  getPageBlob: (
+    index: number,
+    options?: { scale?: number },
+  ) => Promise<Blob | null>;
+  getAllMarkData: () => ShapeDataType[][];
+  setMarkData: (page: number, markData: ShapeDataType[]) => void;
+  setAllMarkData: (markData: ShapeDataType[][]) => void;
+  clearAllMarkData: () => void;
+};
 
 export type PageViewport = {
   height: number;
@@ -24,6 +43,7 @@ export type PDFViewerContextType = {
   zoom: number;
   setZoom: (zoom: number) => void;
   changePage: (page: number, anim?: boolean) => void;
+  pdfViewer: PDFViewerHandle;
 };
 
 const PDFViewerContext = React.createContext<PDFViewerContextType>({
@@ -32,6 +52,30 @@ const PDFViewerContext = React.createContext<PDFViewerContextType>({
   zoom: 0,
   setZoom: () => {},
   changePage: () => {},
+  pdfViewer: {
+    async load() {},
+    changePage() {},
+    getZoom() {
+      return 0;
+    },
+    clearAllMarkData() {},
+    getAllMarkData() {
+      return [];
+    },
+    async getPageBlob() {
+      return null;
+    },
+    getCurrentPage() {
+      return 0;
+    },
+    getPageCount() {
+      return 0;
+    },
+    scrollTo() {},
+    setAllMarkData() {},
+    setMarkData() {},
+    setZoom() {},
+  },
 });
 
 export default PDFViewerContext;
