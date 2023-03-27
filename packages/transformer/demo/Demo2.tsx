b97692ac5056616acd6463618dfee78ae1334589
Kam <331677620@qq.com>
2023-03-27 11:27:20 +0800

    docs: update docs

--- packages/transformer/demo/Demo2.tsx
+++ packages/transformer/demo/Demo2.tsx
@@ -67,7 +67,7 @@ const Demo = () => {
       >
         Add Box
       </Button>
-      <TransformerLayout data={data} onDataChange={setData} style={{ position: 'relative', height: 300, backgroundColor: '#EEE' }}>
+      <TransformerLayout data={data} onDataChange={setData} rotateEnable style={{ position: 'relative', height: 300, backgroundColor: '#EEE' }}>
         {({ color = '#999' }) => <div style={{ height: '100%', backgroundColor: color }} />}
       </TransformerLayout>
     </>
