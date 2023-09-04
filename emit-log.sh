16eddd289c9656d290d2bc623a572b61bf1adbc5
Kam <331677620@qq.com>
2023-09-04 23:23:35 +0800

    chore: update publish config

---
+++ emit-log.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# 取出 log.txt 的文件内容
+log=$(cat log.txt|grep 'New tag:')
+
+# 如果 log.txt 为空
+if [ -z "$log" ]; then
+  curl "https://oapi.dingtalk.com/robot/send?access_token=${DING_TOKEN}" \
+   -H 'Content-Type: application/json' \
+   -d '{"msgtype": "markdown","markdown": {"title":"orca-fe 发布通知","text":"### @orca-fe/pocket\n\n流水线结束，没有新的模块发布"}}'
+  exit 0
+fi
+
+# 如果 log.txt 不为空，则将 log 作为消息发出去，需要将 log 的换行符替换为 \n\n
+log=$(echo "$log"|sed 's/\n/\\n\\n/g')
+curl "https://oapi.dingtalk.com/robot/send?access_token=${DING_TOKEN}" \
+   -H 'Content-Type: application/json' \
+   -d '{"msgtype": "markdown","markdown": {"title":"orca-fe 发布通知","text":"### @orca-fe/pocket\n\n流水线结束，以下模块已发布：\n\n'"$log"'"}}'
+
